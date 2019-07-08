428ce46f75c64d11bcb29988128c36d252c3394a
LaureenMartina <L.martina@protonmail.com>
2019-07-08 22:38:29 +0200

    delete artist folder

--- app/src/main/java/com/example/lays/deezingmusic/artist/DeezerArtist.kt
+++
@@ -1,3 +0,0 @@
-package com.example.lays.deezingmusic.artist
-
-data class DeezerArtist(val id: String, val name: String, val link: String, val picture: String, val nb_album: Int, val nb_fan: String, val tracklist: String)
